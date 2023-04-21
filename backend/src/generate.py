ed42280e6406bc4e118f5a952d244abfdeea2a5d
Ted Vu <levuminhhuy99@gmail.com>
2023-04-22 00:07:59 +0700

    [Feature] send code to backend api

---
+++ backend/src/generate.py
@@ -0,0 +1,24 @@
+from classes import GenerationData
+from model.codegen import CodeGenerationPipeline
+
+def code_generation(data = GenerationData):
+    model_pipeline = CodeGenerationPipeline(data.model_data)
+    model_pipeline.load_generation_pipe()
+    
+    gen_kwargs = {
+        "do_sample": True,
+        "temperature": 0.8,
+        "max_new_tokens": 150,
+        "top_p": 0.9,
+        "top_k": 0,
+        "pad_token_id": pipe.tokenizer.pad_token_id if pipe.tokenizer.pad_token_id else pipe.tokenizer.eos_token_id,
+        "eos_token_id": pipe.tokenizer.eos_token_id,
+        "num_completions": 1
+    }
+    
+    code_gens = model_pipeline.run_code_generation(data.prompt, **gen_kwargs)
+
+    if code_gens:
+        return code_gens[0]
+    else:
+        return "Generation failed !"
