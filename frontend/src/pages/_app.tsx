5703038acf0c4d05be062b7e80eb975c64335c32
Ted Vu <levuminhhuy99@gmail.com>
2023-04-22 16:33:55 +0700

    [Feature] send code to backend api

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default MyApp;
