360accd8c2a90d43905f3f1c8a3a8ad9c31b74e1
Ted Vu <levuminhhuy99@gmail.com>
2023-04-30 15:24:31 +0700

    Upgrade new version

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -276,8 +276,8 @@ const Home: NextPage = () => {
                   <button
                     type="button"
                     onClick={() => {
-                      generate(codeGenerated);
                       setContinueCount((prev: number) => prev + 1);
+                      generate(codeGenerated);
                     }}
                     className="mb-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
                   >
